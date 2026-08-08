d4379b571230e223cc0dc3008d2b21cbdd1d84e9
Sk Ayaan Alam <skayaanalam78@gmail.com>
2026-08-08 14:44:50 +0530

    feat: add admin login and signup forms with basic functionality

---
+++ frontend/src/page/auth/AdminLogin.tsx
@@ -0,0 +1,11 @@
+import AdminLoginForm from "../../components/auth/AdminLoginForm";
+
+function AdminLogin() {
+  return (
+    <div className="admin-login-page">
+      <AdminLoginForm />
+    </div>
+  );
+}
+
+export default AdminLogin;
---
+++ frontend/src/page/auth/AdminSignup.tsx
@@ -0,0 +1,11 @@
+import AdminSignupForm from "../../components/auth/AdminSignupForm";
+
+function AdminSignup() {
+  return (
+    <div className="admin-signup-page">
+      <AdminSignupForm />
+    </div>
+  );
+}
+
+export default AdminSignup;
